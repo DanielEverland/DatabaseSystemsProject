f1f5ca9c35495472a2547b30ad435ae0c9d5af6e
Daniel Everland <danieleverland@gmail.com>
2022-03-24 11:13:36 +0100

    Created templates for creating and populatin relation schemas

---
+++ CreateDatabase.sql
@@ -0,0 +1,71 @@
+# Main script that creates the database
+DROP DATABASE IF EXISTS airport;
+CREATE DATABASE airport;
+use airport;
+
+# =======================================================
+# =============== Create Relation Schemas ===============
+# =======================================================
+# TimeZone
+DROP TABLE IF EXISTS TimeZone;
+# CREATE TABLE
+
+# City
+DROP TABLE IF EXISTS City;
+# CREATE TABLE
+
+# Airport
+DROP TABLE IF EXISTS Airport;
+# CREATE TABLE
+
+# Gate
+DROP TABLE IF EXISTS Gate;
+# CREATE TABLE
+
+# Ticket
+DROP TABLE IF EXISTS Ticket;
+# CREATE TABLE
+
+# Ticket Price
+DROP TABLE IF EXISTS TicketPrice;
+# CREATE TABLE
+
+# Nationality
+DROP TABLE IF EXISTS Nationality;
+# CREATE TABLE
+
+# Passenger
+DROP TABLE IF EXISTS Passenger;
+# CREATE TABLE
+
+# Crew
+DROP TABLE IF EXISTS Crew;
+# CREATE TABLE
+
+# CrewFlight
+DROP TABLE IF EXISTS CrewFlight;
+# CREATE TABLE
+
+# Flight
+DROP TABLE IF EXISTS Flight;
+# CREATE TABLE
+
+# Aircraft Model
+DROP TABLE IF EXISTS AircraftModel;
+# CREATE TABLE
+
+# Aircraft Instance
+DROP TABLE IF EXISTS AircraftInstance;
+# CREATE TABLE
+
+# License
+DROP TABLE IF EXISTS License;
+# CREATE TABLE
+
+# Pilot
+DROP TABLE IF EXISTS Pilot;
+# CREATE TABLE
+
+# Pilot Flight
+DROP TABLE IF EXISTS PilotFlight
+# CREATE TABLE
---
+++ PopulateDatabase.sql
@@ -0,0 +1,53 @@
+# Populates the database with values
+use airport;
+
+# =======================================================
+# ============== Populate Relation Schemas ==============
+# =======================================================
+# TimeZone
+# INSERT TimeZone VALUES;
+
+# City
+# INSERT City VALUES;
+
+# Airport
+# INSERT Airport VALUES;
+
+# Gate
+# INSERT Gate VALUES;
+
+# Ticket
+# INSERT Ticket VALUES;
+
+# Ticket Price
+# INSERT TicketPrice VALUES;
+
+# Nationality
+# INSERT Nationality VALUES;
+
+# Passenger
+# INSERT Passenger VALUES;
+
+# Crew
+# INSERT Crew VALUES;
+
+# CrewFlight
+# INSERT CrewFlight VALUES;
+
+# Flight
+# INSERT Flight VALUES;
+
+# Aircraft Model
+# INSERT AircraftModel VALUES;
+
+# Aircraft Instance
+# INSERT AircraftInstance VALUES;
+
+# License
+# INSERT License VALUES;
+
+# Pilot
+# INSERT Pilot VALUES;
+
+# Pilot Flight
+# INSERT PilotFlight VALUES;
