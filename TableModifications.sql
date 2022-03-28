9cc7ce6654bef1f9665f03f059bcab9c6d3dd436
s204500 <s204500@student.dtu.dk>
2022-03-28 22:16:51 +0200

    comment

--- TableModifications.sql
+++ TableModifications.sql
@@ -1,4 +1,5 @@
 # Raise prices on all non-purchased tickets for future flights by 5%
+# (Turn off Safe Updates in Preferences > SQL Editor)
 UPDATE Ticket 
 INNER JOIN Flight ON Ticket.flightID = Flight.flightID
 SET basePrice = basePrice * 1.05
