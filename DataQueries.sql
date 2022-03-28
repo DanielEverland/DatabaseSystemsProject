57d8605bde03bbbd0abf615a4dee4856f02ae3ac
Benjamin <s204461@student.dtu.dk>
2022-03-28 21:52:11 +0200

    Update scenario

--- DataQueries.sql
+++ DataQueries.sql
@@ -1,20 +1,20 @@
-# Data queries for different 
+# Data queries for different scenarios
 USE airport;
 
-# Query 1: Find all flight times for a specific passenger
-SELECT flightID, departureDateTimeUTC, arrivalDateTimeUTC
-FROM Passenger NATURAL JOIN Flight
-WHERE passengerID = 3;
+# Query 1: Find all passengers for a specific flight
+SELECT passengerID, firstName, lastName
+FROM Passenger NATURAL JOIN Ticket
+WHERE flightID = 1;
 
-# Query 2: Find the number of different meals for a specific flight
+# Query 2: Find the number of different meals on a specific flight
 SELECT Meal, COUNT(*) AS Meals
 FROM Ticket
 WHERE flightID = 1
-AND meal != 'None'
+AND meal IS NOT NULL
 GROUP BY meal;
 
 # Query 3: Find all pilots with an active (not expired) license for a certain AircraftModel
 SELECT pilotID, firstName, lastName, (licenseDurationDays - DATEDIFF(CURRENT_DATE, lastRenewal)) AS 'Days to expiration'
 FROM AircraftModel NATURAL JOIN License NATURAL JOIN Pilot
 WHERE modelName = 'A330'
-AND licenseDurationDays >= DATEDIFF(CURRENT_DATE, lastRenewal);
+AND licenseDurationDays > DATEDIFF(CURRENT_DATE, lastRenewal);
