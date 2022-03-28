37665e7da655075af811ee6120265a10ec36eb9c
s204500 <s204500@student.dtu.dk>
2022-03-28 22:10:58 +0200

    Update and delete statements

---
+++ TableModifications.sql
@@ -0,0 +1,9 @@
+# Raise prices on all non-purchased tickets for future flights by 5%
+UPDATE Ticket 
+INNER JOIN Flight ON Ticket.flightID = Flight.flightID
+SET basePrice = basePrice * 1.05
+WHERE Flight.DepartureDateTimeUTC > NOW() && ISNULL(Ticket.passengerID);
+
+# Fire Leonardo Di Caprio
+DELETE FROM Crew
+WHERE crewID = 1
